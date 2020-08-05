dc0d74ad0b3b1f1c89fa97f89028ed29df3645bb
Giovanni Grieco <giovanni@grieco.dev>
2020-08-05 12:29:13 +0200

    Added useful scripts

---
+++ tools/clean-results.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+rm results/*.{pcap,log,xml}
---
+++ tools/prepare-ns3.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+####
+# Clone ns3 using convient git flags.
+# Also setup symbolic links automatically to include IoD Sim modules
+####
+
+git clone                           \
+  --branch=ns-3.29                  \
+  --depth=1                         \
+  git@gitlab.com:nsnam/ns-3-dev.git \
+  ns3
+
+ln -s ../../drone ns3/src/drone
+ln -s ../../report ns3/src/report
