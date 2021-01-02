45f014dbff5fdc097a5dc5cc12c9523e9ab723d7
Michele Abruzzese <33634641+michezio@users.noreply.github.com>
2021-01-02 23:43:17 +0100

    Update parametric-speed-flight.cc
    In method UpdateDistance for the analytic polynomial integration algorithm was used (order - i +1) but order is instanciated equal to m_speedParams.size() so it's already one more than the real order of the polynomial so (order - i) is correct. Moreover the creation of a new vector to store the modified coefficients was not necessary and a direct multiplication by (order - i) is more efficient.
    
    Also declaring the double y without initialization was yielding some strange behaviours in the Newton-Raphson method (in version 31+) so now it is initialized to 0.

--- drone/model/parametric-speed-flight.cc
+++ drone/model/parametric-speed-flight.cc
@@ -111,7 +111,7 @@ ParametricSpeedFlight::UpdateSpeed (const double &t) const
   const uint32_t order = m_speedParams.size ();
 
   for (uint32_t i = 0; i < order; i++)
-    speed += m_speedParams[i] * std::pow (t, order - 1 - i);
+    speed += m_speedParams[i] * std::pow (t, order - i - 1);
 
   m_currentSpeed = speed;
 }
@@ -120,15 +120,11 @@ void
 ParametricSpeedFlight::UpdateDistance (const double &t) const
 {
   // analytic polynomial integration
-  const uint32_t order = m_speedParams.size ();
-  std::vector<double> coeffs = m_speedParams;
   double distance;
-
+  const uint32_t order = m_speedParams.size ();
+  
   for (uint32_t i = 0; i < order; i++)
-    {
-      coeffs[i] /= (order - i + 1);
-      distance += coeffs[i] * std::pow (t, order - i + 1);
-    }
+      distance += m_speedParams[i] * (order - i) * std::pow (t, order - i);
 
   m_currentDistance = distance;
 }
@@ -184,7 +180,7 @@ Quadratic (double x, void *params)
 {
   std::vector<double> coeffs = ((PrivFuncParams *) params)->GetCoeffs ();
   uint32_t order = coeffs.size () - 1;
-  double y;
+  double y = 0;
 
   for (uint32_t i = 0; i < coeffs.size (); i++)
     y += coeffs[i] * std::pow (x, order - i);
@@ -197,7 +193,7 @@ QuadraticDeriv (double x, void *params)
 {
   std::vector<double> coeffs = ((PrivFuncParams *) params)->GetCoeffsDeriv ();
   uint32_t order = coeffs.size () - 1;
-  double y;
+  double y = 0;
 
   for (uint32_t i = 0; i < coeffs.size (); i++)
     y += coeffs[i] * std::pow (x, order - i);
@@ -247,7 +243,7 @@ ParametricSpeedFlight::FindTime () const
   FDF.fdf = &QuadraticFdf;
   FDF.params = &params;
 
-  T = gsl_root_fdfsolver_steffenson;
+  T = gsl_root_fdfsolver_newton;
   s = gsl_root_fdfsolver_alloc (T);
   gsl_root_fdfsolver_set (s, &FDF, x);
 
