c79f8e1ff86325f936874469b222d41405e6eadb
Giovanni Grieco <giovanni@grieco.dev>
2021-02-28 17:27:16 +0100

    Update ns-3 setup procedure

--- tools/prepare-ns3.sh
+++ tools/prepare-ns3.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/bash
-####
-# Clone ns3 using convient git flags.
-# Also setup symbolic links automatically to include IoD Sim modules
-####
 
-git clone                           \
-  --branch=ns-3.29                  \
-  --depth=1                         \
-  git@gitlab.com:nsnam/ns-3-dev.git \
-  ns3
+git submodule update --init
 
-ln -s ../../drone ns3/src/drone
-ln -s ../../report ns3/src/report
+pushd ns3
+git am ../tools/0001-Integrate-ns-3-with-IoD-Sim.patch
+popd
