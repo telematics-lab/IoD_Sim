0997598b6675566b8b14f28966519f2d0cb312e6
Giovanni Grieco <giovanni@grieco.dev>
2021-07-21 10:30:22 +0200

    Fix patch command errors using wildcard

--- tools/prepare-ns3.sh
+++ tools/prepare-ns3.sh
@@ -3,5 +3,7 @@
 git submodule update --init
 
 pushd ns3
-patch -s -p1 < ../tools/*.patch
+patch -s -p1 < ../tools/0001-Integrate-ns3-with-IoD-Sim.patch
+patch -s -p1 < ../tools/0002-Add-ns3-quirks-for-IoD-Sim-interoperability.patch
+patch -s -p1 < ../tools/0003-Add-LTE-temporary-quirks.patch
 popd
