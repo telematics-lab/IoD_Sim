d5d2ca2ac7d718fefee69f8cb49be43d3b85483b
Giovanni Grieco <giovanni@grieco.dev>
2021-08-05 17:06:45 +0200

    Maximise execution compatibility of script
    This is the first script that a newcomer would execute in its system to properly build IoD Sim. To this end, it is preferable if we ease him some tasks by checking that everything is ok to run this command without issues.

--- tools/prepare-ns3.sh
+++ tools/prepare-ns3.sh
@@ -1,4 +1,19 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+function check_availability() {
+  PRG_NAME=$1
+
+  which $PRG_NAME &> /dev/null
+  if [ "$?" -ne 0 ]; then
+    echo "The program \"${PRG_NAME}\" is not available in your system. Please install it, then re-run this script."
+    exit 1
+  fi
+}
+
+# check dependencies
+check_availability git
+check_availability patch
+
 
 git submodule update --init
 
