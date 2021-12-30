cb9c0b4a90db2c32e392091c46c4ce754cef5f8d
Giovanni Grieco <giovanni@grieco.dev>
2021-12-30 13:41:03 +0100

    get_cellid_per_drone: fix and refactor code

--- analysis/get_cellid_per_drone.py
+++ analysis/get_cellid_per_drone.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python
 import csv
-import sys
 from argparse import ArgumentParser
 
 P = ArgumentParser(description="Parse LTE Rlc Ul Stats file and output one file per drone/IMSI containing the LTE Cell ID")
-
 P.add_argument('rlc_filepath', type=str, help='LTE Rlc Ul Stats file path')
 P.add_argument('output_dir', type=str, help='Output directory where new CSV files will be stored')
+args = P.parse_args()
 
-P.parse_args()
-
-rlc_fp = P.rlc_filepath
-out_csv_dir = P.output_dir
-
-COL_START = 0
-COL_END = 1
+COL_START  = 0
+COL_END    = 1
 COL_CELLID = 2
-COL_IMSI = 3
+COL_IMSI   = 3
+OUT_FILE_PREFIX = 'lte-rlc-drone'
 
 samples = []
 
 # Read everything and agglomerate data in samples
-with open(rlc_fp, 'r') as rlcf:
+with open(args.rlc_filepath, 'r') as rlcf:
   csvr = csv.reader(rlcf, delimiter='\t')
 
   next(csvr) # Discard header
@@ -39,7 +34,7 @@ with open(rlc_fp, 'r') as rlcf:
 # for each imsi in samples, write csv
 i_drone = 0
 for d in samples:
-  with open(f'{out_csv_dir}/lte-rlc-drone-{i_drone}.csv', 'w') as f:
+  with open(f'{args.output_dir}/{OUT_FILE_PREFIX}-{i_drone}.csv', 'w') as f:
     csvw = csv.writer(f)
     csvw.writerow(['start', 'end', 'cellId']) # header
     csvw.writerows(d)
