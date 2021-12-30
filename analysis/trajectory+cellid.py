d78d8c73e820f7a15987a959bcbcebab5053d3f1
Giovanni Grieco <giovanni@grieco.dev>
2021-12-30 15:16:42 +0100

    Add arg parsing and fix naming for traj. parser

--- analysis/trajectory+cellid.py
+++ analysis/trajectory+cellid.py
@@ -1,21 +1,29 @@
+#!/usr/bin/env python
 import csv
+from argparse import ArgumentParser
 
-base_results_dir = 'results/paper_2-2021-12-21.17-19-01'
-n_drones = 5 # quanti droni?
+P = ArgumentParser(description='Combine LTE RLC data and trajectory in a single CSV per drone')
+P.add_argument('n_drones', type=int, help='Number of simulated drones')
+P.add_argument('results_dir', type=str, help='Results directory of reference')
+args = P.parse_args()
 
-for i_drone in range(n_drones):
+INPUT_LTE_RLC_FILE_PREFIX = 'lte-rlc-drone'
+INPUT_TRAJECTORY_FILE_PREFIX = 'trajectory-drone'
+OUTPUT_FILE_PREFIX = 'traj+cellid-drone'
+
+for i_drone in range(args.n_drones):
   start = -1.0
   cellid = 0
 
-  rlcf = open(f'{base_results_dir}/lte-rlc-drone-{i_drone}.csv', 'r')
+  rlcf = open(f'{args.results_dir}/{INPUT_LTE_RLC_FILE_PREFIX}_{i_drone}.csv', 'r')
   rlc_csvr = csv.reader(rlcf)
   next(rlc_csvr) # discard header
 
-  trf = open(f'{base_results_dir}/{i_drone}.csv', 'r')
+  trf = open(f'{args.results_dir}/{INPUT_TRAJECTORY_FILE_PREFIX}_{i_drone}.csv', 'r')
   trf_csvr = csv.reader(trf)
   next(trf_csvr) # discard header
 
-  destf = open(f'{base_results_dir}/traj+cellid-drone-{i_drone}.csv', 'w')
+  destf = open(f'{args.results_dir}/{OUTPUT_FILE_PREFIX}_{i_drone}.csv', 'w')
   dest_csvw = csv.writer(destf)
   dest_csvw.writerow(['x', 'y', 'z', 't', 'cellId'])
 
@@ -29,10 +37,10 @@ for i_drone in range(n_drones):
       rlc_row = rlc_csvr.__next__()
       start = float(rlc_row[0])
       cellid = int(rlc_row[2])
-      print(rlc_row, start, cellid)
 
     dest_csvw.writerow([x, y, z, t, cellid])
 
   destf.close()
   trf.close()
   rlcf.close()
+
