6e06c5a95ce65c71be6475b5ef8f16d0121e7806
Giovanni Grieco <giovanni@grieco.dev>
2021-12-30 14:04:29 +0100

    Add LTE throughtput calculator for each drone

--- analysis/lte-throughput_per_drone.py
+++ analysis/lte-throughput_per_drone.py
@@ -1,16 +1,24 @@
+#!/usr/bin/env python
 import csv
+from argparse import ArgumentParser
 
-base_results_dir = 'results/paper_2-2021-12-21.17-19-01'
-n_drones = 5 # quanti droni?
+P = ArgumentParser(description='Parse LTE MAC Ul stats per drone to evaluate its throughput')
+P.add_argument('n_drones', type=int, help='Number of simulated drones')
+P.add_argument('results_dir', type=str, help='Results directory of reference')
+P.add_argument('--use_Mbps', action='store_true', default=False, help='Report throughput in Mbps. Default: kbps')
+args = P.parse_args()
+
+INPUT_CSV_PREFIX  = 'lte-UlMacStats-drone'
+OUTPUT_CSV_PREFIX = 'lte-throughput-drone' 
 
 drones = []
 
-for i_drone in range(n_drones):
+for i_drone in range(args.n_drones):
   drones.append([])
   txbytes = 0
   timeref = 0.0
 
-  with open(f'{base_results_dir}/lte-DlMacStats-drone_{i_drone}.csv') as csvf:
+  with open(f'{args.results_dir}/{INPUT_CSV_PREFIX}_{i_drone}.csv') as csvf:
     csvr = csv.reader(csvf)
     next(csvr) # skip heading
 
@@ -21,7 +29,8 @@ for i_drone in range(n_drones):
       #drones[i_drone].append([time, cellid, sizetb])
 
       if int(stime) > timeref:
-        drones[i_drone].append([int(timeref), scellid, txbytes * 8.0 / 1024.0])
+        exp = 2 if args.use_Mbps else 1 # wether to use kbps or Mbps
+        drones[i_drone].append([int(timeref), scellid, txbytes * 8.0 / (1024.0 ** exp)])
         timeref = stime
         txbytes = 0.0
 
@@ -29,8 +38,10 @@ for i_drone in range(n_drones):
 
 
 for i_drone in range(len(drones)):
-  with open(f'{base_results_dir}/lte-throughput-drone_{i_drone}.csv', 'w') as csvf:
+  with open(f'{args.results_dir}/{OUTPUT_CSV_PREFIX}_{i_drone}.csv', 'w') as csvf:
     csvw = csv.writer(csvf)
 
-    csvw.writerow(['time', 'cellid', 'Mbps']) # header
+    hdr_scale = 'Mbps' if args.use_Mbps else 'kbps'
+    csvw.writerow(['time', 'cellid', hdr_scale]) # header
     csvw.writerows(drones[i_drone])
+
