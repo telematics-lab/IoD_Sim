a308fe7dec4d8d7f656e422a91ab65c8b493599c
Giovanni Grieco <giovanni.grieco@poliba.it>
2022-03-04 11:23:56 +0000

    Fix LTE latency analyser

--- analysis/lte_latency.py
+++ analysis/lte_latency.py
@@ -1,19 +1,22 @@
 #%%
 import re
 
+import numpy as np
 import pandas as pd
 import plotly.express as px
 
 #%%
 base_results_dir = '../results/'
 scenario_name = 'paper_3-lte'
-scenario_dir = f'{scenario_name}-2022-02-28.18-13-32'
+scenario_dir = f'{scenario_name}-2022-03-03.20-38-18'
+tx_trace_filename = 'paper_3-lte.log'
 
-tx_trace_filename = 'paper_3-test.log'
+lte_id2addr_offset = 2
 
-rex = re.compile(r'(?P<time>[0-9\.+]+)s (?P<nodeid>[0-9]+) .+ 200\.0\.0\.1 .+ ns3::SeqTsHeader \(\(seq=(?P<sn>[0-9]+).+')
+rex = re.compile(r'(?P<time>[0-9\.+]+)s (?P<nodeid>[0-9]+) .+ 200\.0\.0\.1 .+ '
+                 r'ns3::SeqTsHeader \(\(seq=(?P<sn>[0-9]+).+')
 
-tx_traces_raw = {}
+tx_traces = {}
 pkts = []
 
 with open(f'{base_results_dir}/{scenario_dir}/{tx_trace_filename}', 'r') as f:
@@ -25,54 +28,68 @@ with open(f'{base_results_dir}/{scenario_dir}/{tx_trace_filename}', 'r') as f:
       # print(r.groupdict())
       # break
       ##
-      pkts.append({k: float(v) for k,v in r.groupdict().items()})
+      g = r.groupdict()
+      pkts.append({
+        'time': float(g['time']),
+        'nodeid': int(g['nodeid']),
+        'sn': int(g['sn'])
+      })
 
 for p in pkts:
-  if p['nodeid'] in tx_traces_raw:
-    tx_traces_raw[p['nodeid']].append(p)
-  else:
-    tx_traces_raw[p['nodeid']] = [p]
+  lte_host_addr = f'7.0.0.{p["nodeid"] + lte_id2addr_offset}'
 
-tx_traces = []
-for k,v in tx_traces_raw.items():
-  tx_traces.append(pd.DataFrame(v))
+  if lte_host_addr in tx_traces:
+    tx_traces[lte_host_addr].append(p)
+  else:
+    tx_traces[lte_host_addr] = [p]
+
+for k in tx_traces.keys():
+  tx_traces[k] = pd.DataFrame(tx_traces[k])
+
 
 #%%
-internet_rx_trace_filename = 'internet-25-1.tr'
+internet_rx_trace_filename = 'internet-37-1.tr'
+ip_addrs = [f'7.0.0.{i}' for i in range(lte_id2addr_offset, len(tx_traces.keys()) + 1)]
 
-rx_traces = []
+rex = re.compile(r'r (?P<time>[0-9\.+]+).+7\.0\.0\.(?P<lte_host_id>[0-9]+).+'
+                 r' > 1337.+ns3::SeqTsHeader \(\(seq=(?P<sn>[0-9]+).+')
+rx_traces = {k: [] for k in tx_traces.keys()}
 
-ip_addr_offset = 2
-ip_addrs = [f'7.0.0.{i}' for i in range(ip_addr_offset, len(tx_traces_raw.keys()) + 1)]
+with open(f'{base_results_dir}/{scenario_dir}/{internet_rx_trace_filename}', 'r') as f:
+  for l in f:
+    r = rex.match(l)
+    if r is not None:
+      ## DEBUG
+      # print(r[0])
+      # print(r.groupdict())
+      # break
+      ##
+      g = r.groupdict()
+      host_addr = f'7.0.0.{g["lte_host_id"]}'
 
-for lte_ip in ip_addrs:
-  rex = re.compile(fr'r (?P<time>[0-9\.+]+).+{lte_ip}.+ > 1337.+ns3::SeqTsHeader \(\(seq=(?P<sn>[0-9]+).+')
-  pkts = []
+      rx_traces[host_addr].append({
+        'time': float(g['time']),
+        'sn': int(g['sn'])
+      })
 
-  with open(f'{base_results_dir}/{scenario_dir}/{internet_rx_trace_filename}', 'r') as f:
-    for l in f:
-      r = rex.match(l)
+rx_traces = {k: pd.DataFrame(v) for k,v in rx_traces.items()}
 
-      if r is not None:
-        ## DEBUG
-        # print(r[0])
-        # print(r.groupdict())
-        # break
-        ##
-        pkts.append({k: float(v) for k,v in r.groupdict().items()})
-        pkts[-1]['ipaddr'] = lte_ip
-
-  rx_traces.append(pd.DataFrame(pkts))
-
-# %%
-latency = {}
-
-for i in range(len(tx_traces[2:])):
-  tx = tx_traces[i][['time', 'sn']]
-  rx = rx_traces[i][['time', 'sn']]
-  lat = pd.merge(tx, rx, on=['sn', 'sn'])
-  latency[rx_traces[i]['ipaddr'][0]] = (lat['time_y'] - lat['time_x']) * 1e-3
 
 # %%
-fig = px.box(latency, labels=dict(value='Latency [ms]', variable='GU IP Address'))
+def to_latency(txdf, rxdf):
+  if txdf.empty or rxdf.empty:
+    return pd.Series({}, dtype=np.float64)
+
+  l = pd.merge(txdf, rxdf, on='sn')
+  return (l['time_y'] - l['time_x'])
+
+assert(tx_traces.keys() == rx_traces.keys())
+latency = {k: to_latency(tx_traces[k], rx_traces[k])
+           for k in tx_traces.keys()}
+
+# %%
+fig = px.box(latency,
+             labels=dict(value='Latency [s]', variable='GU IP Address'),
+             log_y=True)
 fig.show()
+# %%
