6ec87959d7a56f3faed076fefeffe9844799fe6e
dpugliese6 <daniele.pugliese@poliba.it>
2025-11-21 10:20:22 -0500

    Build IoD_Sim in debug mode and fix test executable

--- tools/configure-iodsim.sh
+++ tools/configure-iodsim.sh
@@ -9,6 +9,6 @@ pip install -r analysis/requirements.txt \
             -r scenario/py_design_support/requirements.txt
 
 pushd ns3
-./ns3 configure --build-profile=optimized --enable-examples --enable-tests --disable-mpi \
+./ns3 configure --build-profile=debug --enable-examples --enable-tests --disable-mpi \
                 --disable-python --enable-modules=iodsim,nr,leo,point-to-point-layout
 popd
